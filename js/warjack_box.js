fa75bc8ca29c554cb5cbcb7ac0a1db843b3f9adb
hans0126 <hayashi0126@gmail.com>
2015-11-10 16:59:54 +0800

    twin table

--- js/warjack_box.js
+++ js/warjack_box.js
@@ -3,48 +3,108 @@
      var _self = this;
 
      var animeRequest;
-
+     _self.init = init;
      _self.getBrush = getBrush;
      _self.resetTable = resetTable;
      _self.mirrorMode = false;
      _self.getDataArray = getDataArray;
      _self.transferAndRender = transferAndRender;
+     _self.changeMode = changeMode;
+     _self.displayMode = 0; //0:warjack 1:colossus
 
      var stage = new PIXI.Container(),
-         table = new PIXI.Container();
+         table = new PIXI.Container(),
+         table2 = new PIXI.Container()
 
      var cubeWidth = 40,
          cubeHeight = 40;
 
      var arrData = [];
-
      var mirror = [5, 3, 1, -1, -3, -5];
-
+     var reverseMirror = [-5, -3, -1, 1, 3, 5];
      var brushType = null;
-
-
      var paintStart = false;
+     var cubeColor = [0x666666, 0xFFFFFF, 0xcccccc]; //disable,normal,hover
+     var areaWidth = 360;
+     var areaHeight = 360;
 
-     _self.init = function() {
-         editAreaRenderer = new PIXI.autoDetectRenderer(360, 360);
+     function init() {
+         var _areaWidth = areaWidth
+         if (_self.displayMode == 1) {
+             _areaWidth = areaWidth * 2;
+         }
+
+         editAreaRenderer = new PIXI.autoDetectRenderer(_areaWidth, areaHeight);
          document.getElementById("c").appendChild(editAreaRenderer.view);
 
-         table.interactive = true;
+         table.interactive = table2.interactive = true;
          table.x = 20;
-         table.y = 20;
+         table.y = table2.y = 20;
+         table2.x = 380;
 
-
-
-
-         table.mousedown = function() {
+         table.mousedown = table2.mousedown = function() {
+             if (!brushType) return
              paintStart = true;
          }
 
-         table.mouseup = function() {
+         table.mouseup = table.mouseout = table2.mouseup = table2.mouseout = function() {
              paintStart = false;
          }
 
+         createCubes(table);
+         createCubes(table2);
 
+         stage.addChild(table);
+         stage.addChild(table2);
+
+         table.hitArea = new PIXI.Rectangle(0, 0, table.width, table.height);
+         table2.hitArea = new PIXI.Rectangle(0, 0, table2.width, table2.height);
+
+         table.idName = "table";
+         table2.idName = "table2";
+
+         if (_self.displayMode == 0) {
+             table2.visible = false;
+         }
+
+         anime();
+     }
+
+     function penDown(event) {
+         if (!brushType) return
+
+         anime();
+         this.dragging = true;
+         var newPosition = event.data.getLocalPosition(this);
+         painProcess(newPosition.x, newPosition.y);
+     }
+
+     function painting(event) {
+         if (this.dragging) {
+             var newPosition = event.data.getLocalPosition(this);
+             painProcess(newPosition.x, newPosition.y);
+         }
+     }
+
+     function penUp() {
+         this.dragging = false;
+         console.log("A");
+         window.cancelRequestAnimFrame(animeRequest);
+     }
+
+     function changeMode(_mode) {
+         _self.displayMode = _mode
+         if (_mode == 0) {
+             table2.visible = false;
+             editAreaRenderer.resize(areaWidth, areaHeight);
+         } else {
+             table2.visible = true;
+             editAreaRenderer.resize(areaWidth * 2, areaHeight);
+         }
+     }
+
+
+     function createCubes(_table) {
          for (var i = 0; i < 6; i++) {
              for (var j = 0; j < 6; j++) {
 
@@ -75,7 +135,7 @@
 
                  cube.blockType = "enabled";
                  cube.blockContent = '';
-                 cube.origanlColor = 0x334455;
+                 cube.origanlColor = cubeColor[0];
 
                  graphic.beginFill(cube.origanlColor);
                  graphic.drawRect(0, 0, cubeWidth, cubeHeight);
@@ -85,85 +145,53 @@
                  cube.addChild(txt);
 
                  cube.mousedown = function() {
+                     if (!brushType) return
                      painProcess(this);
                  }
 
                  cube.mouseover = function(event) {
-                     var _this = this.getChildAt(0);
-                     _this.clear();
-                     _this.beginFill(0xcccccc);
-                     _this.lineStyle(2, 0xFF0000);
-                     _this.drawRect(0, 0, cubeWidth, cubeHeight);
-                     _this.endFill();
+                     reDrawCubeBg(this.getChildAt(0), cubeColor[2], true);
+
                      if (paintStart) {
                          painProcess(this);
                      }
-
                  }
 
                  cube.mouseout = function(event) {
-                     var _this = this.getChildAt(0);
-                     _this.clear();
-                     _this.beginFill(this.origanlColor);
-                     _this.drawRect(0, 0, cubeWidth, cubeHeight);
-                     _this.endFill();
+                     reDrawCubeBg(this.getChildAt(0), this.origanlColor);
                  }
 
-                 table.addChild(cube);
+                 _table.addChild(cube);
              }
          }
-
-         stage.addChild(table);
-
-         table.hitArea = new PIXI.Rectangle(0, 0, table.width, table.height);
-
-         anime();
-         // editAreaRenderer.render(stage);
      }
 
-     function penDown(event) {
-
-         if (!brushType) return
-
-         anime();
-         this.dragging = true;
-         var newPosition = event.data.getLocalPosition(this);
-         painProcess(newPosition.x, newPosition.y);
-
-     }
-
-     function painting(event) {
-         if (this.dragging) {
-             var newPosition = event.data.getLocalPosition(this);
-             painProcess(newPosition.x, newPosition.y);
-
-
-         }
-     }
-
-     function penUp() {
-         this.dragging = false;
-         console.log("A");
-         window.cancelRequestAnimFrame(animeRequest);
-     }
 
      function painProcess(_cube) {
          var _targetCube = _cube;
          var _mirrorCube;
          var _mirrorIndex
          var _mirrorReverseText;
-
-         var _color = [0x666666, 0xFFFFFF];
-
+         var _currentColor;
+         
          if (_self.mirrorMode) {
-             var i = table.getChildIndex(_cube);
-             if (i < 6) {
-                 _mirrorIndex = mirror[i];
-             } else {
-                 _mirrorIndex = mirror[i % 6];
-             }
+             var _idx;
+             if (_self.displayMode == 0) {
+                 _idx = table.getChildIndex(_cube);
+                 _mirrorIndex = getMirrorIdx(_idx);
+                 _mirrorCube = table.getChildAt(_idx + _mirrorIndex);
 
-             _mirrorCube = table.getChildAt(i + _mirrorIndex);
+             } else {
+                 if (_cube.parent.idName == "table") {
+                     _idx = table.getChildIndex(_cube);
+                     _mirrorIndex = getMirrorIdx(_idx);
+                     _mirrorCube = table2.getChildAt(_idx + _mirrorIndex);
+                 } else {
+                     _idx = table2.getChildIndex(_cube);
+                     _mirrorIndex = getMirrorIdx(_idx, true);
+                     _mirrorCube = table.getChildAt(_idx - _mirrorIndex);
+                 }
+             }
 
              switch (brushType.font) {
                  case "L":
@@ -188,33 +216,29 @@
          }
 
          if (brushType.bType == 'disabled') {
-             reDrawCubeBg(_targetCube.getChildAt(0), _color[0]);
-             _targetCube.getChildAt(1).visible = false;
+
+             _currentColor = cubeColor[0];
+
+             reDrawCubeBg(_targetCube.getChildAt(0), _currentColor, true);
+             drawSymbol(_targetCube.getChildAt(1), '', false);
 
              if (_self.mirrorMode) {
-                 reDrawCubeBg(_mirrorCube.getChildAt(0), _color[0]);
-                 _mirrorCube.getChildAt(1).visible = false;
+                 reDrawCubeBg(_mirrorCube.getChildAt(0), _currentColor);
+                 drawSymbol(_mirrorCube.getChildAt(1), '', false);
              }
 
          } else {
+             _currentColor = cubeColor[1];
 
-             reDrawCubeBg(_targetCube.getChildAt(0), _color[1]);
-
+             reDrawCubeBg(_targetCube.getChildAt(0), _currentColor, true);
              if (_self.mirrorMode) {
-                 reDrawCubeBg(_mirrorCube.getChildAt(0), _color[1]);
+                 reDrawCubeBg(_mirrorCube.getChildAt(0), _currentColor);
              }
 
              if (brushType.font) {
-                 _targetCube.getChildAt(1).visible = true;
-                 _targetCube.getChildAt(1).text = brushType.font;
-                 _targetCube.getChildAt(1).x = cubeWidth / 2 - _targetCube.getChildAt(1).width / 2;
-                 _targetCube.getChildAt(1).y = cubeHeight / 2 - _targetCube.getChildAt(1).height / 2;
-
+                 drawSymbol(_targetCube.getChildAt(1), brushType.font, true);
                  if (_self.mirrorMode) {
-                     _mirrorCube.getChildAt(1).visible = true;
-                     _mirrorCube.getChildAt(1).text = _mirrorReverseText;
-                     _mirrorCube.getChildAt(1).x = cubeWidth / 2 - _mirrorCube.getChildAt(1).width / 2;
-                     _mirrorCube.getChildAt(1).y = cubeHeight / 2 - _mirrorCube.getChildAt(1).height / 2;
+                     drawSymbol(_mirrorCube.getChildAt(1), _mirrorReverseText, true);
                  }
 
              } else {
@@ -227,44 +251,64 @@
 
          _targetCube.blockType = brushType.bType;
          _targetCube.blockContent = brushType.font;
-         _targetCube.origanlColor = _targetCube.getChildAt(0).tint;
-         console.log( _targetCube.origanlColor);
+         _targetCube.origanlColor = _currentColor;
+
          if (_self.mirrorMode) {
              _mirrorCube.blockType = brushType.bType;
              _mirrorCube.blockContent = _mirrorReverseText;
-             _mirrorCube.origanlColor = _mirrorCube.getChildAt(0).tint;
+             _mirrorCube.origanlColor = _currentColor;
+         }
+
+         function getMirrorIdx(_idx, _reverse) {
+
+             var _index
+             if (_idx < 6) {
+                 if (_reverse) {
+                     _index = reverseMirror[_idx];
+                 } else {
+                     _index = mirror[_idx];
+                 }
+
+             } else {
+                 if (_reverse) {
+                     _index = reverseMirror[_idx % 6];
+                 } else {
+                     _index = mirror[_idx % 6];
+                 }
+             }
+             return _index
          }
 
      }
 
      function resetTable() {
          for (var i = 0; i < table.children.length; i++) {
-             table.children[i].getChildAt(0).tint = 0x666666;
-             table.children[i].getChildAt(1).visible = false;
-             table.children[i].blockType = null;
-             table.children[i].blockContent = null;
+             var _cube = table.getChildAt(i);
+             reDrawCubeBg(_cube.getChildAt(0), cubeColor[0]);
+
+             _cube.getChildAt(1).visible = false;
+             _cube.blockType = null;
+             _cube.blockContent = null;
+             _cube.origanlColor = cubeColor[0];
          }
      }
+
 
      function getBrush(_b) {
 
          switch (_b) {
              case 'disabled':
-
                  brushType = {
                      bType: _b,
                      font: ""
                  }
-
                  break;
 
              case 'enabled':
-
                  brushType = {
                      bType: _b,
                      font: ""
                  }
-
                  break;
 
              default:
@@ -273,7 +317,6 @@
                      font: _b
                  }
          }
-
      }
 
      function getDataArray() {
@@ -304,7 +347,6 @@
          return _arr
      }
 
-
      function transferAndRender(_data) {
          var _reg = /[clmrgi+-]/ig;
          var _data = _data.split('\n'),
@@ -326,7 +368,6 @@
              }
          }
 
-
          for (var i = 0; i < _reData.length; i++) {
              for (j = 0; j < _reData[i].length; j++) {
                  var _color,
@@ -335,11 +376,11 @@
                      _fontVisible;
 
                  if (_reData[i][j] == "-") {
-                     _color = 0x666666;
+                     _color = cubeColor[0];
                      _text = '';
                      _fontVisible = false;
                  } else {
-                     _color = 0xffffff;
+                     _color = cubeColor[1];
                      _text = "";
                      _fontVisible = true;
                      if (_reData[i][j] != "+") {
@@ -347,11 +388,8 @@
                      }
                  }
 
-                 _cube.getChildAt(0).tint = _color;
-                 _cube.getChildAt(1).visible = _fontVisible;
-                 _cube.getChildAt(1).text = _text;
-                 _cube.getChildAt(1).x = cubeWidth / 2 - _cube.getChildAt(1).width / 2;
-                 _cube.getChildAt(1).y = cubeHeight / 2 - _cube.getChildAt(1).height / 2;
+                 reDrawCubeBg(_cube.getChildAt(0), _color);
+                 drawSymbol(_cube.getChildAt(1), _text, _fontVisible);
 
                  colCounter++;
              }
@@ -360,11 +398,21 @@
          editAreaRenderer.render(stage);
      }
 
-     function reDrawCubeBg(_cube, _color) {
+     function reDrawCubeBg(_cube, _color, _line) {
          _cube.clear();
          _cube.beginFill(_color);
+         if (_line) {
+             _cube.lineStyle(2, 0xFF0000);
+         }
          _cube.drawRect(0, 0, cubeWidth, cubeHeight);
          _cube.endFill();
+     }
+
+     function drawSymbol(_symbol, _text, _visible) {
+         _symbol.visible = _visible;
+         _symbol.text = _text;
+         _symbol.x = cubeWidth / 2 - _symbol.width / 2;
+         _symbol.y = cubeHeight / 2 - _symbol.height / 2;
      }
 
      function anime() {
